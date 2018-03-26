6711d1a82a82c596c3c89df4ddc5ae27da314453
Kevin Ma <kbma.kevin@gmail.com>
2018-03-25 20:30:11 -0400

    add ui styling by ostap

--- WebContent/partials/footer.jsp
+++ WebContent/partials/footer.jsp
@@ -1,5 +1,6 @@
-    <!-- Footer -->
-    <footer class="py-3 bg-dark">
+  <!-- Footer -->
+    <div class="push"></div>
+    <footer class="page-footer py-3 bg-dark">
       <div class="container">
         <p class="text-center text-white">Copyright &copy; KOPS Dev team. 2018</p>
       </div>
--- WebContent/scripts/core/app.js
+++ WebContent/scripts/core/app.js
@@ -1,3 +1,20 @@
 /**
  * add custom javascript here
  */
+
+/* function to show product rating using stars */
+$.fn.rating = function() {
+    return $(this).each(function() {
+        // Get the value of rating
+        var ratingVal = parseFloat($(this).html());
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, ratingVal))) * 16;
+        var $span = $('<span />').width(size);
+        // Replace the numerical value with stars
+        $(this).html($span);
+    });
+}
+
+$(function() {
+    $('span.stars').rating();
+});
